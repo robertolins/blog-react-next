af8a0629bba58a984846edc951e58d7cecd1be14
Roberto Lima <robertolimalins@gmail.com>
2026-02-13 15:38:04 -0300

    feat: cria o componente de loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-import clsx from 'clsx';
+import { SpinLoader } from '@/components/SpinLoader';
 
 export default function HomePage() {
   return (
     <div>
-      <h1
-        className={clsx(
-          'text-6xl',
-          'font-bold',
-          'text-blue-500',
-          'hover:text-blue-50',
-          'hover:bg-blue-500',
-          'transition',
-          'duration-1000',
-        )}
-      >
-        Texto no meu h1
-      </h1>
+      <SpinLoader />
     </div>
   );
 }
