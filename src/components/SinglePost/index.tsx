9c1d7900eee685d1f02933bd45973e2456a19088
Roberto Lima <robertolimalins@gmail.com>
2026-02-18 16:49:42 -0300

    feat: prepera a página do post para receber um markdown

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { findPostBySlugCached } from '@/lib/post/queries';
 import { PostHeading } from '../PostHeading';
 import { PostDate } from '../PostDate';
+import { SafeMarkdown } from '../SafeMarkdown';
 
 type SinglePostProps = {
   slug: string;
@@ -30,7 +31,7 @@ export async function SinglePost({ slug }: SinglePostProps) {
 
       <p className='text-xl mb-4 text-slate-600'>{post.excerpt}</p>
 
-      <div>{post.content}</div>
+      <SafeMarkdown markdown={post.content} />
     </article>
   );
 }
