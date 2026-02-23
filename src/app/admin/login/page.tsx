617614405f7d12057b5767ab0018b11fedfc0db9
Roberto Lima <robertolimalins@gmail.com>
2026-02-23 11:13:22 -0300

    feat: prepara as páginas da área administrativa

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,5 @@
+export const dynamic = 'force-dynamic';
+
+export default async function AdminLoginPage() {
+  return <div className='py-2 text-5xl'>AdminLoginPage</div>;
+}
---
+++ src/app/admin/post/page.tsx
@@ -0,0 +1,5 @@
+export const dynamic = 'force-dynamic';
+
+export default async function AdminPostPage() {
+  return <div className='py-2 text-5xl'>AdminPostPage</div>;
+}
