2056e75a44fa7ab694b2f44ab92da666c5f285ca
Roberto Lima <robertolimalins@gmail.com>
2026-02-25 00:22:40 -0300

    feat: cria componente para campo de texto

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,54 +1,52 @@
-import { Button } from '@/components/Button';
-import { BugIcon } from 'lucide-react';
+import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <>
-      <div className='py-16 flex gap-4 flex-wrap items-center'>
-        <Button variant='default' size='sm'>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant='ghost' size='md'>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant='danger' size='lg'>
-          <BugIcon />
-          Confirma
-        </Button>
-      </div>
-      <div className='py-16 flex gap-4 flex-wrap items-center'>
-        <Button variant='default' size='sm' disabled>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant='ghost' size='md' disabled>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant='danger' size='lg' disabled>
-          <BugIcon />
-          Confirma
-        </Button>
-      </div>
-
-      <div className='py-16 flex gap-4 flex-wrap items-center'>
-        <Button variant='default' size='sm' className='w-full'>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant='default' size='md' className='w-full'>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant='default' size='lg' className='w-full'>
-          <BugIcon />
-          Confirma
-        </Button>
-      </div>
-    </>
+    <div className='flex flex-col gap-6'>
+      <InputText
+        labelText='Nome'
+        placeholder='Digite seu nome'
+        type='password'
+      />
+      <InputText
+        labelText='Inline'
+        placeholder='Digite seu nome'
+        type='text'
+        inline
+        className='w-full'
+      />
+      <InputText
+        labelText='Inline'
+        placeholder='Digite seu nome'
+        type='text'
+        inline
+        className='w-75'
+      />
+      <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+      <InputText
+        disabled
+        labelText='Sobrenome'
+        placeholder='Digite seu sobrenome'
+        defaultValue='Olá mundo'
+      />
+      <InputText
+        disabled
+        labelText='Sobrenome'
+        placeholder='Digite seu sobrenome'
+      />
+      <InputText
+        labelText='Sobrenome'
+        placeholder='Digite seu sobrenome'
+        readOnly
+      />
+      <InputText
+        labelText='Sobrenome'
+        placeholder='Digite seu sobrenome'
+        defaultValue='Olá mundo'
+        readOnly
+      />
+    </div>
   );
 }
