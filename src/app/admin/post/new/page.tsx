4c18d338fa3d76f2e1af56f1b4c093e05828d189
Roberto Lima <robertolimalins@gmail.com>
2026-02-24 23:40:22 -0300

    feat: cria componente para botões

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,54 @@
+import { Button } from '@/components/Button';
+import { BugIcon } from 'lucide-react';
+
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
-  return <div className='py-2 text-5xl'>AdminPostNewPage</div>;
+  return (
+    <>
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm'>
+          <BugIcon />
+          Confirma
+        </Button>
+        <Button variant='ghost' size='md'>
+          <BugIcon />
+          Confirma
+        </Button>
+        <Button variant='danger' size='lg'>
+          <BugIcon />
+          Confirma
+        </Button>
+      </div>
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm' disabled>
+          <BugIcon />
+          Confirma
+        </Button>
+        <Button variant='ghost' size='md' disabled>
+          <BugIcon />
+          Confirma
+        </Button>
+        <Button variant='danger' size='lg' disabled>
+          <BugIcon />
+          Confirma
+        </Button>
+      </div>
+
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm' className='w-full'>
+          <BugIcon />
+          Confirma
+        </Button>
+        <Button variant='default' size='md' className='w-full'>
+          <BugIcon />
+          Confirma
+        </Button>
+        <Button variant='default' size='lg' className='w-full'>
+          <BugIcon />
+          Confirma
+        </Button>
+      </div>
+    </>
+  );
 }
