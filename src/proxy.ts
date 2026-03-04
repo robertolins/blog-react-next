c7bf91f3f113479a2a8f5c1d4765a9b6a1ee0b94
Roberto Lima <robertolimalins@gmail.com>
2026-03-04 18:00:26 -0300

    feat: adiciona um middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyJwt } from './lib/login/manage-login';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const isLoginPage = request.nextUrl.pathname.startsWith('/admin/login');
   const isAdminPage = request.nextUrl.pathname.startsWith('/admin');
   const isGetRequest = request.method === 'GET';
--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,7 +1,6 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { drizzleDb } from '@/db/drizzle';
-import { logColor } from '@/utils/log-color';
 import { asyncDelay } from '@/utils/async-delay';
 import { postsTable } from '@/db/drizzle/schemas';
 import { eq } from 'drizzle-orm';
@@ -11,7 +10,6 @@ const waitInMs = Number(process.env.SIMULATE_WAIT_IN_MS) || 0;
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
     await asyncDelay(waitInMs, true);
-    logColor('findAllPublic', Date.now());
 
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
@@ -23,7 +21,6 @@ export class DrizzlePostRepository implements PostRepository {
 
   async findAll(): Promise<PostModel[]> {
     await asyncDelay(waitInMs, true);
-    logColor('findAll', Date.now());
 
     const posts = await drizzleDb.query.posts.findMany();
 
@@ -32,7 +29,6 @@ export class DrizzlePostRepository implements PostRepository {
 
   async findById(id: string): Promise<PostModel> {
     await asyncDelay(waitInMs, true);
-    logColor('findById', Date.now());
 
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq }) => eq(posts.id, id),
@@ -45,7 +41,6 @@ export class DrizzlePostRepository implements PostRepository {
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
     await asyncDelay(waitInMs, true);
-    logColor('findBySlugPublic', Date.now());
 
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq, and }) =>
